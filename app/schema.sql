a60aaea2f066fe1adafd24bdccd163614e039296
rogerioshieh <40327446+rogerioshieh@users.noreply.github.com>
2019-11-21 15:53:20 -0500

    Create ingredients blueprint

---
+++ app/schema.sql
@@ -0,0 +1,61 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS meal;
+DROP TABLE IF EXISTS recipe;
+DROP TABLE IF EXISTS ingredient;
+DROP TABLE IF EXISTS mealRecipeRelationship;
+DROP TABLE IF EXISTS recipeIngredientRelationship;
+
+
+--meals, recipes, and ingredients are unique to each user. 
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+--
+CREATE TABLE meal (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE recipe (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  author_id INTEGER NOT NULL,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES user (id)
+);
+
+CREATE TABLE ingredient (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT NOT NULL,
+  portion_size FLOAT(2) NOT NULL,
+  portion_size_unit TEXT CHECK ( portion_size_unit in ('g', 'kg', 'oz', 'lb', 'cup', 'ml', 'l', 'gal', 'T', 't', 'in', 'unit') ) NOT NULL,
+  protein FLOAT(1) NOT NULL,
+  fat FLOAT(1) NOT NULL,
+  carbs FLOAT(1) NOT NULL,
+  notes TEXT
+);
+
+--these are the junction tables
+CREATE TABLE mealRecipeRelationship(
+    mealID INTEGER NOT NULL,
+    recipeID INTEGER NOT NULL,
+    FOREIGN KEY (mealID) REFERENCES meal(id), 
+    FOREIGN KEY (recipeID) REFERENCES recipe(id),
+    UNIQUE (mealID, recipeID)
+);
+
+CREATE TABLE recipeIngredientRelationship(
+    recipeID INTEGER NOT NULL,
+    ingredientID INTEGER NOT NULL,
+    quantity FLOAT(2) NOT NULL,
+    units TEXT CHECK ( units in ('g', 'kg', 'oz', 'lb', 'cup', 'ml', 'l', 'gal', 'T', 't', 'in', 'unit') ) NOT NULL,
+    FOREIGN KEY (recipeID) REFERENCES recipe(id),
+    FOREIGN KEY (ingredientID) REFERENCES ingredient(id),
+    UNIQUE (recipeID, ingredientID)
+);
